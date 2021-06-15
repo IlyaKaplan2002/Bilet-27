336aea5e5d0b4ffa7b4c87550a9d2ac656c646c7
Ilya Kaplan <kaplan11092002@gmail.com>
2021-06-15 10:20:50 +0300

    unittests complete

--- unittest_third_task.py
+++ unittest_third_task.py
@@ -1,9 +1,14 @@
 import unittest
 from third_task import read_file
 
+
 class TestReadFile(unittest.TestCase):
-    def test_read_file(self):
-        self.assertEqual(read_file(), "1u 0v 4w") 
+    def test_read_file_1(self):
+        self.assertEqual(read_file(), "1u 0v 4w")
+
+    def test_read_file_2(self):
+        self.assertEqual(read_file("data/new_data.txt"), "29u 3v 0w")
+
 
 if __name__ == '__main__':
     unittest.main()
